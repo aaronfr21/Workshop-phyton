c53bb726e011069620c5f5568f1cde49e0e6e308
aaronfr21 <aaronmailopuw@gmail.com>
2022-02-28 16:09:25 +0700

    Create More on Conditions.py

---
+++ minggu-03/src/More on Conditions.py
@@ -0,0 +1,4 @@
+# It is possible to assign the result of a comparison or other Boolean expression to a variable
+string1, string2, string3 = '', 'Trondheim', 'Hammer Dance'
+non_null = string1 or string2 or string3
+non_null
