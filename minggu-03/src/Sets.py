03faa976c34adc6a6fdee19372671e6bf3442553
aaronfr21 <aaronmailopuw@gmail.com>
2022-02-28 16:07:02 +0700

    Create Sets.py

---
+++ minggu-03/src/Sets.py
@@ -0,0 +1,26 @@
+# Here is a brief demonstration
+basket = {'apple', 'orange', 'apple', 'pear', 'orange', 'banana'}
+print(basket)                      # show that duplicates have been removed
+
+'orange' in basket                 # fast membership testing
+
+'crabgrass' in basket
+
+
+# Demonstrate set operations on unique letters from two words
+
+a = set('abracadabra')
+b = set('alacazam')
+a                                  # unique letters in a
+
+a - b                              # letters in a but not in b
+
+a | b                              # letters in a or b or both
+
+a & b                              # letters in both a and b
+
+a ^ b                              # letters in a or b but not both
+
+# Similarly to list comprehensions, set comprehensions are also supported:
+a = {x for x in 'abracadabra' if x not in 'abc'}
+a
