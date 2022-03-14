aa96b302305334ec4bd6d5b0d8b980818ad88875
aaronfr21 <aaronmailopuw@gmail.com>
2022-03-14 23:42:18 +0700

    workshop python pertemuan 5

---
+++ minggu-05/src/Old string formatting.py
@@ -0,0 +1,5 @@
+# This operation is commonly known as string interpolation
+import math
+print('The value of pi is approximately %5.3f.' % math.pi)
+# output
+The value of pi is approximately 3.142.
