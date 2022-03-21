b9b81c4cbc2720e25ba317b398803a1a93cd2386
aaronfr21 <aaronmailopuw@gmail.com>
2022-03-21 23:30:40 +0700

    workshop python pertemuan 6

---
+++ minggu-06/src/Syntax Errors.py
@@ -0,0 +1,6 @@
+# Syntax errors, also known as parsing errors
+while True print('Hello world')
+  File "<stdin>", line 1
+    while True print('Hello world')
+                   ^
+SyntaxError: invalid syntax
