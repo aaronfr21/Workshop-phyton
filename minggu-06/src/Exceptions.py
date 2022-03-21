4949f58350311bcd9c112a89712ca440bddf5536
aaronfr21 <aaronmailopuw@gmail.com>
2022-03-21 23:32:47 +0700

    workshop python pertemuan 6

---
+++ minggu-06/src/Exceptions.py
@@ -0,0 +1,13 @@
+# Errors detected during execution are called exceptions and are not unconditionally fatal
+10 * (1/0)
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+ZeroDivisionError: division by zero
+4 + spam*3
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+NameError: name 'spam' is not defined
+'2' + 2
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+TypeError: can only concatenate str (not "int") to str
