ba8e5e1a61f13e52dc4ee7fd2071f2dd1b6b04e6
aaronfr21 <aaronmailopuw@gmail.com>
2022-03-29 10:36:05 +0700

    workshop python pertemuan 7

---
+++ minggu-07/src/9.2.1. Scopes and Namespaces Example.py
@@ -0,0 +1,29 @@
+# This is an example demonstrating how to reference the different scopes and namespaces
+def scope_test():
+    def do_local():
+        spam = "local spam"
+
+    def do_nonlocal():
+        nonlocal spam
+        spam = "nonlocal spam"
+
+    def do_global():
+        global spam
+        spam = "global spam"
+
+    spam = "test spam"
+    do_local()
+    print("After local assignment:", spam)
+    do_nonlocal()
+    print("After nonlocal assignment:", spam)
+    do_global()
+    print("After global assignment:", spam)
+
+scope_test()
+print("In global scope:", spam)
+
+# output
+After local assignment: test spam
+After nonlocal assignment: nonlocal spam
+After global assignment: nonlocal spam
+In global scope: global spam
