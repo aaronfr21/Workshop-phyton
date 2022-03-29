54bcb6bf3a42c6741131564d987214d3122acc68
aaronfr21 <aaronmailopuw@gmail.com>
2022-03-29 10:42:39 +0700

    workshop python pertemuan 7

---
+++ minggu-07/src/9.5. Inheritance.py
@@ -0,0 +1,10 @@
+# The syntax for a derived class definition looks like this
+class DerivedClassName(BaseClassName):
+    <statement-1>
+    .
+    .
+    .
+    <statement-N>
+
+# The name BaseClassName must be defined in a scope containing the derived class definition
+class DerivedClassName(modname.BaseClassName):
