221feac8e9cbd9c23e35ef805bf2b4ed9fbbed66
aaronfr21 <aaronmailopuw@gmail.com>
2022-05-24 03:28:04 +0700

    workshop python pertemuan 11

---
+++ minggu-11/src/Installation.py
@@ -0,0 +1,32 @@
+# installation
+
+##Create an environment
+# Windows
+'''
+> mkdir myproject
+> cd myproject
+> py -3 -m venv venv
+'''
+
+# macOS/Linux
+'''
+$ mkdir myproject
+$ cd myproject
+$ python3 -m venv venv
+'''
+
+##Activate the environment
+# Windows
+'''
+> venv\Scripts\activate
+'''
+
+# macOS/Linux
+'''
+$ . venv/bin/activate
+'''
+
+##Install Flask
+'''
+$ pip install Flask
+'''
